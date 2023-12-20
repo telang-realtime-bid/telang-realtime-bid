897057c21b152d6a98f0cdd571958b8a3fe7a609
faisalyudiansah <mfaisal.yudiansah@gmail.com>
2023-12-20 09:58:32 +0700

    fix: fixed card on homepage

--- client/src/components/CardHome.jsx
+++ client/src/components/CardHome.jsx
@@ -6,14 +6,14 @@ const CardHome = ({ auction }) => {
   }
   return (
     <>
-      <div className="card w-80 shadow-xl relative cursor-pointer">
-        <div className="overlay absolute top-0 left-0 right-0 bottom-0 rounded-3xl bg-gray-900 opacity-10"></div>
-        <img src={auction.imageUrl} alt="picture" className='rounded-3xl h-96' />
+      <div className="card w-72 shadow-xl bg-base-300 p-5 rounded-3xl relative cursor-pointer">
+        {/* <div className="overlay absolute top-0 left-0 right-0 bottom-0 rounded-3xl bg-gray-900 opacity-10"></div> */}
+        <img src={auction.imageUrl} alt="picture" className='rounded-3xl h-72' />
         <div className="overlay-content absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center text-white">
-          <h2 className="text-lg text-center p-10 font-bold mb-2">{auction.name}</h2>
-          <p className="text-lg mb-4 font-bold">{setCurrency(auction.currentBid)}</p>
-          <button className="btn text-base bg-base-100 hover:bg-base-300 rounded-xl">Bid</button>
+          <p className="text-lg mb-12 text-center font-bold">{setCurrency(auction.currentBid)}</p>
+          <button className="btn text-base bg-base-300 hover:bg-base-100 rounded-xl">Bid</button>
         </div>
+        <h2 className="text-md text-center text-base-secondary p-7 font-thin">{auction.name}</h2>
       </div>
     </>
   )
--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <>
-            <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+            <footer className="footer footer-center p-10 bg-base-300 text-base-content rounded">
 
                 <nav className="grid grid-flow-col gap-4">
                     <a href="/" className="link link-hover">Home</a>
